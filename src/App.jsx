ab3435e136463573cb088ed6039561f03e8d9cff
abdullatif61 <abdullatifozturk616@gmail.com>
2023-06-03 01:19:21 +0300

    localStorage-add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [yazi, setYazi] = useState("");
   const [liste, setListe] = useState([]);
+  const [loc, setLoc] = useState(localStorage.getItem("liste"));
+  
+
+
+  
+  useEffect(() => { 
+    if (loc !== null ){
+
+      setListe(JSON.parse(loc));
+    }
+
+  },[] );
+  useEffect(() => {
+   
+
+      localStorage.setItem("liste", JSON.stringify(liste));
+    
+
+
+  }, [liste]);
 
   function sil(id) {
     const yeniListe = liste.filter((item) => item.id !== id);
@@ -22,49 +42,50 @@ function App() {
       value: yazi,
     };
     setListe((eskideger) => [...eskideger, eleman]);
-    console.log(liste);
     setYazi("");
   }
   return (
-      <div className="flex py-6 px-4 flex-col mx-auto sm:w-[600px] w-screen gap-y-3 sm:h-[600px] h-screen max-h-full overflow-auto bg-orange-300 ">
-        <h1 className="text-3xl text-center text-slate-500">To Do List</h1>
-        <div className="w-full mx-auto flex items-center text-center  ">
-          <input
-            value={yazi}
-            onChange={(gelen) => setYazi(gelen.target.value)}
-            className=" w-full   border-4 rounded-xl  mx-3 "
-            type="text"
-            placeholder="not giriniz"
-          />
-          <button
-            className="text-xl hover:bg-slate-700 transition-all  rounded-full px-2 py-1 bg-slate-400"
-            onClick={ekle}
-          >
-            Add
-          </button>
-        </div>
-        <div className="w-full">
-          <ul>
-            {liste.map((eleman) => {
-              return (
-                <li className="" key={eleman.id}>
-                  <div className="flex bg-slate-300    rounded-xl my-4 px-4">
-                    <div className="mx-2 text-slate-600   w-full">{eleman.value}</div>
-                    <div>
-                      <button
-                        className="text-white rounded-3xl hover:text-black transition-all  px-1 bg-red-600 "
-                        onClick={() => sil(eleman.id)}
-                      >
-                        X
-                      </button>
-                    </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+    <div className="flex py-6 px-4 flex-col mx-auto sm:w-[600px] w-screen gap-y-3 sm:h-[600px] h-screen max-h-full overflow-auto bg-orange-300 ">
+      <h1 className="text-3xl text-center text-slate-500">To Do List</h1>
+      <div className="w-full mx-auto flex items-center text-center  ">
+        <input
+          value={yazi}
+          onChange={(gelen) => setYazi(gelen.target.value)}
+          className=" w-full   border-4 rounded-xl  mx-3 "
+          type="text"
+          placeholder="not giriniz"
+        />
+        <button
+          className="text-xl hover:bg-slate-700 transition-all  rounded-full px-2 py-1 bg-slate-400"
+          onClick={ekle}
+        >
+          Add
+        </button>
       </div>
+      <div className="w-full">
+        <ul>
+          {liste.map((eleman) => {
+            return (
+              <li className="" key={eleman.id}>
+                <div className="flex bg-slate-300    rounded-xl my-4 px-4">
+                  <div className="mx-2 text-slate-600   w-full">
+                    {eleman.value}
+                  </div>
+                  <div>
+                    <button
+                      className="text-white rounded-3xl hover:text-black transition-all  px-1 bg-red-600 "
+                      onClick={() => sil(eleman.id)}
+                    >
+                      X
+                    </button>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 }
 
